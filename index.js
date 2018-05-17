87ac7d47fd9dcc997f9e32af480d9d3271ee4aa0
Leadrive <gzakay@163.com>
2018-05-17 22:38:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
  // @flow
 
-import React, {
-  PropTypes,
-} from 'react';
+import React from 'react';
 import {
   View,
 } from 'react-native';
+import PropTypes from 'prop-types'
 
 import getBlocks from './src/getBlocks';
 
